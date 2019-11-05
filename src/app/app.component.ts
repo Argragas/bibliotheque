4e4936801a956175f3e4b330701f4f7bfb17ede3
Argragas <avezard.jeason@gmail.com>
2019-11-05 09:43:53 +0100

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,7 @@ import * as firebase from 'firebase';
 export class AppComponent {
   constructor() {
     const config = {
-        apiKey: 'AIzaSyBeF5xV8U9NuIOyqEkMNk0GrzdzxeDSZq4',
-        authDomain: 'angularopenclassroom-14a51.firebaseapp.com',
-        databaseURL: 'https://angularopenclassroom-14a51.firebaseio.com',
-        projectId: 'angularopenclassroom-14a51',
-        storageBucket: 'angularopenclassroom-14a51.appspot.com',
-        messagingSenderId: '83496881656',
-        appId: '1:83496881656:web:90019abb9ade2994'
+        
       };
     firebase.initializeApp(config);
   }
