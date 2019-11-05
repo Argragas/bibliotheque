8cca348b41b4cb791151ba007d62b68d15ef47de
Argragas <avezard.jeason@gmail.com>
2019-11-05 09:10:45 +0100

    add signin signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bibliotheque';
+  constructor() {
+    const config = {
+        apiKey: 'AIzaSyBeF5xV8U9NuIOyqEkMNk0GrzdzxeDSZq4',
+        authDomain: 'angularopenclassroom-14a51.firebaseapp.com',
+        databaseURL: 'https://angularopenclassroom-14a51.firebaseio.com',
+        projectId: 'angularopenclassroom-14a51',
+        storageBucket: 'angularopenclassroom-14a51.appspot.com',
+        messagingSenderId: '83496881656',
+        appId: '1:83496881656:web:90019abb9ade2994'
+      };
+    firebase.initializeApp(config);
+  }
 }
